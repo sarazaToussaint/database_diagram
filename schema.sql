8f897dd105fcfd5e0c44ee373dbd2123979ad90a
sarazaToussaint <tmsaraza@gmail.com>
2022-07-20 16:26:26 +0200

    Create tables

--- schema.sql
+++ schema.sql
@@ -1,5 +1,21 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE patients (
+    id int PRIMARY KEY,
+    name varchar,
+    date_of_birth date,
+);
+
+
+CREATE TABLE medical_histories (
+    id int PRIMARY KEY,
+    admitted_at timestamp,
+    patient_id int FOREIGN KEY REFERENCES patients(id),
+    status varchar,
+);
+
+CREATE TABLE treatments (
+    id int PRIMARY KEY,
+    type varchar,
+    name varchar,
 );
